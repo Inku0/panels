6ad8414828e94ab24245311e162b0823cf18e6bf
inku0 <ingvar@saarend.com>
2026-06-15 13:07:16 +0300

    optimized imports and added exceptions

--- shelly.py
+++ shelly.py
@@ -1,17 +1,19 @@
 from time import sleep
-import logging
-import requests
-import json
+from json import loads, dumps
+from requests import post
+from logging import getLogger
 
 
 ERR_NO_DEVICES = "no devices available, try adding some with add_devices()"
+
+# TODO: this should be refactored so that a class instance corresponds to a device, not a huge controller class
 
 class Shelly:
   def __init__(self, server: str, key: str) -> None:
     self.server: str = server
     self.key: str = key
     self.devices: list[str] = []
-    self.logger = logging.getLogger(__name__)
+    self.logger = getLogger(__name__)
 
   def add_devices(self, devices: list[str]) -> None:
     """
@@ -26,22 +28,20 @@ class Shelly:
     A device's ID is the key, its online status as a boolean is the value.
     """
     if not self.devices:
-      self.logger.error(ERR_NO_DEVICES)
-      return {}
+      raise Exception(ERR_NO_DEVICES)
 
     data = {"ids":self.devices,"select":["status"],"pick":{"status":["sys"]}}
-    r = requests.post(
+    r = post(
       url=f"{self.server}/v2/devices/api/get?auth_key={self.key}",
       headers={"Content-Type": "application/json"},
-      data=json.dumps(data)
+      data=dumps(data)
     )
 
     if r.status_code != 200:
-      self.logger.error(f"shelly returned an error: {r.text}")
-      return {}
+      raise Exception(f"shelly returned an error: {r.text}")
 
     online: dict[str, bool] = {}
-    for device in json.loads(r.text):
+    for device in loads(r.text):
       online[device["id"]] = (device["online"] == 1)
 
     return online
@@ -52,23 +52,21 @@ class Shelly:
     A device's ID is the key, its active status (i.e., whether it's working) as a boolean is the value.
     """
     if not self.devices:
-      self.logger.error(ERR_NO_DEVICES)
-      return {}
+      raise Exception(ERR_NO_DEVICES)
 
     data = {"ids":self.devices,"select":["status"],"pick":{"status":["switch:1"]}}
     # switch:1 seems to control the actual power output, not sure what switch:0 is for
-    r = requests.post(
+    r = post(
       url=f"{self.server}/v2/devices/api/get?auth_key={self.key}",
       headers={"Content-Type": "application/json"},
-      data=json.dumps(data)
+      data=dumps(data)
     )
 
     if r.status_code != 200:
-      self.logger.error(f"shelly returned an error: {r.text}")
-      return {}
+      raise Exception(f"shelly returned an error: {r.text}")
 
     active: dict[str, bool] = {}
-    for device in json.loads(r.text):
+    for device in loads(r.text):
       active[device["id"]] = (device["status"]["switch:1"]["output"] == True)
 
     return active
@@ -80,8 +78,7 @@ class Shelly:
     :param devices: List of devices to turn on or off. If unset, then all added devices are turned off.
     """
     if not self.devices:
-      self.logger.error(ERR_NO_DEVICES)
-      return
+      raise Exception(ERR_NO_DEVICES)
 
     if not devices:
       devices = self.devices
@@ -94,12 +91,12 @@ class Shelly:
       # the default channel is 0, but the POST request just didn't do anything when channel = 0
       # more info: the switch seems to have two channels (0 and 1) with separate temperatures and output values
       # not sure what they represent, though
-      r = requests.post(
+      r = post(
         url=f"{self.server}/v2/devices/api/set/switch?auth_key={self.key}",
         headers={"Content-Type": "application/json"},
-        data=json.dumps(data)
+        data=dumps(data)
       )
       if r.status_code != 200:
-        self.logger.error(f"shelly returned an error while turning {"on" if status == True else "off"} device {device}: {r.text}")
-        return
+        raise Exception(f"shelly returned an error while turning {"on" if status == True else "off"} device {device}: {r.text}")
+
       self.logger.info(f"turned {"on" if status == True else "off"} device {device}")
